b597ef1569294b8200912cce04622a1cd6b4d927
EthanCS <ethancs1993@gmail.com>
2024-03-11 12:48:16 +0800

    typemeta

--- crates/lib/luxseed-ecs/src/archetype.rs
+++ crates/lib/luxseed-ecs/src/archetype.rs
@@ -1,8 +1,62 @@
-use std::{any::TypeId, collections::HashMap};
+use std::{alloc::Layout, any::TypeId, collections::HashMap};
 
-#[derive(Copy, Clone)]
+#[derive(Debug, Copy, Clone)]
 pub struct TypeMeta {
     type_id: TypeId,
+    type_name: &'static str,
+    layout: Layout,
+    drop_fn: unsafe fn(*mut u8),
+}
+
+impl TypeMeta {
+    pub fn new<T: 'static>() -> Self {
+        Self {
+            type_id: TypeId::of::<T>(),
+            type_name: std::any::type_name::<T>(),
+            layout: Layout::new::<T>(),
+            drop_fn: |ptr| unsafe { std::ptr::drop_in_place(ptr as *mut T) },
+        }
+    }
+
+    pub fn type_id(&self) -> TypeId {
+        self.type_id
+    }
+
+    pub fn type_name(&self) -> &'static str {
+        self.type_name
+    }
+
+    pub fn layout(&self) -> Layout {
+        self.layout
+    }
+
+    pub fn drop_fn(&self) -> unsafe fn(*mut u8) {
+        self.drop_fn
+    }
+}
+
+impl PartialEq for TypeMeta {
+    fn eq(&self, other: &Self) -> bool {
+        self.type_id == other.type_id
+    }
+}
+
+impl Eq for TypeMeta {}
+
+impl PartialOrd for TypeMeta {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl Ord for TypeMeta {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.layout
+            .align()
+            .cmp(&other.layout.align())
+            .reverse()
+            .then_with(|| self.type_id.cmp(&other.type_id))
+    }
 }
 
 pub struct Archetype {
